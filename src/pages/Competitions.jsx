cd0ebf239ad59727c7d33d2ea917469b33b14e7c
Miroir Raphael <darryl.rik@gmail.com>
2025-09-03 17:11:14 +0200

    Refactor useCompetitions hook to handle optional user ID and update query logic; enhance Competitions page with club addition functionality and permission checks for admin role

--- src/pages/Competitions.jsx
+++ src/pages/Competitions.jsx
@@ -1,6 +1,7 @@
 import supabase from "../lib/supabaseClient";
 import { useSelector } from "react-redux";
 import { useCompetitions } from "../hooks/useCompetitions";
+import { use } from "react";
 
 const Competitions = () => {
   const user = useSelector((state) => state.user) || {};
@@ -22,6 +23,25 @@ const Competitions = () => {
       .insert({ name: "Provinciale Hainaut D3", season: "2025-2026" });
   };
 
+  const handleAddClub = async (competitionId, clubId) => {
+    if (!user) return;
+
+    if (user.role !== "admin-comite") {
+      alert("You do not have permission to add clubs");
+      return;
+    }
+
+    const { error } = await supabase
+      .from("competition_clubs")
+      .insert([{ competition_id: competitionId, club_id: clubId }]);
+
+    if (error) {
+      console.error("Erreur:", error);
+    } else {
+      console.log("Club ajouté à la compétition");
+    }
+  };
+
   return (
     <div>
       <p>Competitions</p>
@@ -35,6 +55,11 @@ const Competitions = () => {
         )}
       </ul>
       <button onClick={handleCreateCompetition}>Create Competition</button>
+      {user.role === "admin-comite" && (
+        <button onClick={() => handleAddClub(competition.id, club.id)}>
+          Add a Club
+        </button>
+      )}
     </div>
   );
 };
